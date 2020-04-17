116aa446782d06b147472c2d834981c0f0162147
Tudor Groza <tudor.groza@pryzm.health>
2020-04-17 10:52:47 +0800

    Reverting changes to use the distributed scheduler

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/scheduler/tasks/SOLRDocumentsStatusCheckTask.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/scheduler/tasks/SOLRDocumentsStatusCheckTask.java
@@ -3,7 +3,6 @@ package uk.ac.ebi.spot.gwas.deposition.scheduler.tasks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.spot.gwas.deposition.config.SystemConfigProperties;
 import uk.ac.ebi.spot.gwas.deposition.constants.PublicationIngestStatus;
@@ -33,7 +32,6 @@ public class SOLRDocumentsStatusCheckTask {
     @Autowired(required = false)
     private SOLRService solrService;
 
-    @Scheduled(cron = "0 */1 * * * ?")
     public void checkSOLRDocuments() {
         if (solrService == null) {
             return;
