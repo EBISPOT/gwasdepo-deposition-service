dffe5a1f1fb325490d8203bf3d1151a62013c6b0
Tudor Groza <tudor.groza@pryzm.health>
2020-04-22 19:46:50 +0800

    Added Completed submissions check.

---
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/scheduler/tasks/CompleteSubmissionsTask.java
@@ -0,0 +1,42 @@
+package uk.ac.ebi.spot.gwas.deposition.scheduler.tasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.spot.gwas.deposition.domain.CompletedSubmission;
+import uk.ac.ebi.spot.gwas.deposition.domain.Publication;
+import uk.ac.ebi.spot.gwas.deposition.repository.CompletedSubmissionRepository;
+import uk.ac.ebi.spot.gwas.deposition.service.PublicationService;
+import uk.ac.ebi.spot.gwas.deposition.service.SummaryStatsProcessingService;
+
+import java.util.List;
+
+@Component
+public class CompleteSubmissionsTask {
+
+    private static final Logger log = LoggerFactory.getLogger(CompleteSubmissionsTask.class);
+
+    @Autowired
+    private PublicationService publicationService;
+
+    @Autowired
+    private CompletedSubmissionRepository completedSubmissionRepository;
+
+    @Autowired
+    private SummaryStatsProcessingService summaryStatsProcessingService;
+
+    public void checkCompletedSubmissions() {
+        log.info("Verifying for completed submissions ...");
+        List<CompletedSubmission> completedSubmissionList = completedSubmissionRepository.findAll();
+        log.info("Found {} completed submissions.", completedSubmissionList.size());
+        for (CompletedSubmission completedSubmission : completedSubmissionList) {
+            Publication publication = publicationService.retrievePublication(completedSubmission.getPublicationId(), true);
+            if (publication != null) {
+                log.info("Calling SS Service to wrap up Globus for publication: {}", publication.getPmid());
+                summaryStatsProcessingService.callGlobusWrapUp(publication);
+            }
+        }
+        log.info("Completed submissions check done.");
+    }
+}
