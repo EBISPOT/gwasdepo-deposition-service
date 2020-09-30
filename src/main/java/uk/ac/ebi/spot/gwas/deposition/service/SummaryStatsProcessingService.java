c4ec01a6dd551e29120ae14816bc20536d0403b7
Tudor Groza <tudor.groza@pryzm.health>
2020-09-30 21:46:22 +0800

    Fixed auto-submit for metadata only submissions.

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/service/SummaryStatsProcessingService.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/service/SummaryStatsProcessingService.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.spot.gwas.deposition.service;
 
+import uk.ac.ebi.spot.gwas.deposition.domain.BodyOfWork;
+import uk.ac.ebi.spot.gwas.deposition.domain.Publication;
 import uk.ac.ebi.spot.gwas.deposition.domain.Submission;
 import uk.ac.ebi.spot.gwas.deposition.domain.SummaryStatsEntry;
 
@@ -7,7 +9,7 @@ import java.util.List;
 
 public interface SummaryStatsProcessingService {
 
-    void processSummaryStats(Submission submission, String fileUploadId, List<SummaryStatsEntry> summaryStatsEntries);
+    void processSummaryStats(Submission submission, String fileUploadId, List<SummaryStatsEntry> summaryStatsEntries, Publication publication, BodyOfWork bodyOfWork);
 
     void callGlobusWrapUp(String submissionId);
 }
