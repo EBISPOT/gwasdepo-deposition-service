fc7a2f9271eff64129e4957dddf80812a5e19a11
Sajo John <sajo@ebi.ac.uk>
2021-03-30 18:30:56 +0100

    GOCI-3 Curator Edit Template

---
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/config/DepositionAuditJaversConfig.java
@@ -0,0 +1,15 @@
+package uk.ac.ebi.spot.gwas.deposition.config;
+
+import org.javers.spring.auditable.AuthorProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import uk.ac.ebi.spot.gwas.deposition.util.SimpalAuthorProvider;
+
+@Configuration
+public class DepositionAuditJaversConfig {
+
+    @Bean
+    public AuthorProvider provideJaversAuthor(){
+        return new SimpalAuthorProvider();
+    }
+}
