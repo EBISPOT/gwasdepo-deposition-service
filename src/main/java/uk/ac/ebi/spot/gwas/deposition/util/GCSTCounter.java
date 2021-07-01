7a2989efcfc88d8059a4ea9158884b4b1dcb57a2
ala <ala@ebi.ac.uk>
2021-07-01 12:30:20 +0100

    change delete and insert to update, counter reset only for dev profile

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/util/GCSTCounter.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/util/GCSTCounter.java
@@ -3,9 +3,12 @@ package uk.ac.ebi.spot.gwas.deposition.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Service;
 import uk.ac.ebi.spot.gwas.deposition.constants.GWASDepositionBackendConstants;
 import uk.ac.ebi.spot.gwas.deposition.domain.GCSTCounterItem;
+import uk.ac.ebi.spot.gwas.deposition.exception.EntityNotFoundException;
 import uk.ac.ebi.spot.gwas.deposition.repository.GCSTCounterItemRepository;
 
 import java.util.List;
@@ -18,17 +21,30 @@ public class GCSTCounter {
     @Autowired
     private GCSTCounterItemRepository gcstCounterItemRepository;
 
+    @Autowired
+    Environment environment;
+
     public synchronized String getNext() {
-        int counter = 90000000;
+
         List<GCSTCounterItem> gcstCounterItemList = gcstCounterItemRepository.findAll();
-        if (!gcstCounterItemList.isEmpty()) {
-            counter = gcstCounterItemList.get(0).getCurrentValue();
+        if (gcstCounterItemList.isEmpty()) {
+            if (environment.acceptsProfiles(Profiles.of("dev"))) {
+                log.info("Current counter: not found in db");
+                int counter = 90000001;
+                GCSTCounterItem gcstCounterItem = gcstCounterItemRepository.insert(new GCSTCounterItem(counter));
+                log.info("Inserted new counter: {} | {}", gcstCounterItem.getId(), gcstCounterItem.getCurrentValue());
+                return GWASDepositionBackendConstants.PREFIX_GCST + gcstCounterItem.getCurrentValue();
+            }
+            log.error("Collection gcstCounter is empty, no entries found.");
+            throw new EntityNotFoundException("Collection gcstCounter is empty, no entries found.");
         }
+        GCSTCounterItem gcstCounterItem = gcstCounterItemList.get(0);
+        int counter = gcstCounterItem.getCurrentValue();
         log.info("Current counter: {}", counter);
         counter++;
-        gcstCounterItemRepository.deleteAll();
-        GCSTCounterItem gcstCounterItem = gcstCounterItemRepository.insert(new GCSTCounterItem(counter));
-        log.info("Inserted new counter: {} | {}", gcstCounterItem.getId(), gcstCounterItem.getCurrentValue());
-        return GWASDepositionBackendConstants.PREFIX_GCST + Integer.toString(gcstCounterItem.getCurrentValue());
+        gcstCounterItem.setCurrentValue(counter);
+        gcstCounterItem = gcstCounterItemRepository.save(gcstCounterItem);
+        log.info("Updated counter: {} with new value: {}", gcstCounterItem.getId(), gcstCounterItem.getCurrentValue());
+        return GWASDepositionBackendConstants.PREFIX_GCST + gcstCounterItem.getCurrentValue();
     }
 }
