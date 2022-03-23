2cdb68d14a60525549dfa06806d07d9d0ee152cf
sajo-ebi <77010716+sajo-ebi@users.noreply.github.com>
2022-03-23 16:49:23 +0000

    Changing DB pointing to Old Mongo Prod DB

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/config/MongoConfig.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/config/MongoConfig.java
@@ -111,7 +111,20 @@ public class MongoConfig {
         @Override
         public MongoClient mongoClient() {
             String mongoUri = systemConfigProperties.getMongoUri();
-            return new MongoClient(new MongoClientURI("mongodb://" + mongoUri));
+            String dbUser = systemConfigProperties.getDbUser();
+            String dbPassword = systemConfigProperties.getDbPassword();
+            String credentials = "";
+            if (dbUser != null && dbPassword != null) {
+                dbUser = dbUser.trim();
+                dbPassword = dbPassword.trim();
+                if (!dbUser.equalsIgnoreCase("") &&
+                        !dbPassword.equalsIgnoreCase("")) {
+                    credentials = dbUser + ":" + dbPassword + "@";
+                }
+            }
+
+            return new MongoClient(new MongoClientURI("mongodb://" + credentials + mongoUri));
+           // return new MongoClient(new MongoClientURI("mongodb://" + mongoUri));
         }
 
         @Bean
